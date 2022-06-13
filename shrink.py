e5358dd502daedbfb2c1c0bdb9343102c1139f8e
approdite <1413170963@qq.com>
2022-06-14 06:17:54 +0800

    feature: shrink packages list at the start of the process

--- shrink.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-if __name__ == '__main__':
-    with open('packages.txt', 'r') as origin:
-        with open('s_packages.txt', 'w') as shrink:
-            for line in origin.readlines():
-                line = line.strip()
-                if not os.path.exists(os.path.join('packages', line)):
-                    shrink.write(line + '\n')
